6cb4fc2f489d437b5906145c95e202c5ee5efb86
Aaron Drobek <adrobek09@gmail.com>
2017-08-03 16:18:19 -0400

    project is due

--- starter_files/scripts/main.js
+++ starter_files/scripts/main.js
@@ -5,12 +5,57 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
+// function assert(hand, value) {
+//   let output = handValue(hand);
+//   console.assert(output === value, `Expecting ${output} to be ${value}`);
+// }
+
+// let expectedOutput =[1, 2, 4, 5, 8, 9];
+//
+// let numbers = [4, 5, 1, 8, 9, 2];
+//
+// function bubbleSort(list) {
+//   let sortedFlag = true;
+//   let resultArray = [];
+//
+//   while(sortedFlag) {
+//     sortedFlag = false;
+//     for (var i = 0; i < list.length-1; i++) {
+//       if (list[i] > list[i+1]) {
+//         sortedFlag = true;
+//         let temp = list[i];
+//         list[i] = list[i+1];
+//         list[i+1] = temp;
+//       }
+//     }
+//   }
+//   return resultArray;
+// }
 
 
-function handValue (hand) {
+// function assert(hand, value)
+
+let value = ["1","2","3","4"]
+let hand = []
+
+function handValue(hand) {
+//   let card = 0;
+// for (var i = 0; i < value.length; i++) {
+//         value[i] += card += 1;
+// }
+  for (var i = 0; i < value.length; i++) {
+        // Math.floor(value[i]);
+
+  }
+
+console.log(Math.floor(value[i]));
+
+
+
+
 
 
   return;
