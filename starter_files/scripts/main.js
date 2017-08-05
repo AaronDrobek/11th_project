08e3085ebfcea6a9793ce12fa72fb32abed1bd70
Aaron Drobek <adrobek09@gmail.com>
2017-08-05 15:49:34 -0400

    sorry I thought I had an incomplete submitted on Friday

--- starter_files/scripts/main.js
+++ starter_files/scripts/main.js
@@ -38,28 +38,86 @@
 
 // function assert(hand, value)
 
-let value = ["1","2","3","4"]
-let hand = []
+// let value = ["1","2","3","4","5","6","7"]
+// let = [];
 
 function handValue(hand) {
-//   let card = 0;
-// for (var i = 0; i < value.length; i++) {
-//         value[i] += card += 1;
-// }
-  for (var i = 0; i < value.length; i++) {
-        // Math.floor(value[i]);
+  // console.log("Your hand is: ", hand);
+  let sum = 0;
 
-  }
+  for (var i = 0; i < hand.length; i++) {
+    // console.log("Looking at: ", hand[i]);
+    //Add the value of the card
+    if (hand[i] === "2") {
+      // console.log("You have a 2");
+      sum = sum + 2;
+    }else if (hand[i] === "3"){
+      // console.log("You have a 3");
+      sum = sum + 3;
+    }else if (hand[i] === "4"){
+      // console.log("You have a 4");
+      sum = sum + 4;
+    }else if (hand[i] === "5"){
+      // console.log("You have a 5");
+      sum = sum + 5;
 
-console.log(Math.floor(value[i]));
+    }else if (hand[i] === "6"){
+      // console.log("You have a 6");
+      sum = sum + 6;
+    }else if (hand[i] === "7"){
+      // console.log("You have a 7");
+      sum = sum + 7;
+    }else if (hand[i] === "8"){
+      // console.log("You have an 8");
+      sum = sum + 8;
+    }else if (hand[i] === "9"){
+      // console.log("You have a 9");
+      sum = sum + 9;
+    }else if (hand[i] === "10"){
+      // console.log("You have a 10");
+      sum = sum + 10;
+    }else if (hand[i] === "J"){
+      // console.log("You have a jack");
+      sum = sum + 10;
 
+    }else if (hand[i] === "Q"){
+      // console.log("You have a queen");
+      sum = sum + 10;
+    }else if (hand[i] === "K"){
+      // console.log("You have a king");
+      sum = sum + 10;
+    }else if (hand[i] === "A"){
+      // console.log("You have an ace");
+      sum = sum + 11;
+    }
+    //Check if value is over 21
+    if (sum > 21){
+      // console.log("The sum is greater than 21");
+      sum = sum - 10;
+    }else if (sum > 24){
+      sum = sum - 10;
 
+      }
+      // console.log("The sum is currently: ", sum);
+    }
 
-
-
-
-  return;
+  // console.log(hand);
+  console.log(sum);
+  // console.log("Returning: ", sum);
+  return sum;
 }
+
+// return hand;
+
+// console.log(sum));
+
+
+
+
+
+
+
+
 
 
 /* -----  Hints ------
